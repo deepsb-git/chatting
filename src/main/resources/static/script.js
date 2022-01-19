635609d9092c4a264f430abc8476d48ed2ed34c7
nareshb-git <nareshchandrabarik58pdp@gmail.com>
2022-01-19 10:18:21 +0530

    Initial commit in chat app

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,74 @@
+let stompClient = null
+
+function sendMessage(message)
+ {
+
+    let jsonOb={
+        name:localStorage.getItem("name"),
+        content:$("#message-value").val()
+    }
+
+    stompClient.send("/app/message",{},JSON.stringify(jsonOb));
+
+}
+
+
+function connect()
+ {
+       let socket=new SockJS("/server")
+       stompClient=Stomp.over(socket)
+       stompClient.connect({},function(frame){
+           console.log("Connected :"+frame)
+
+           $("#name-from").addClass('d-none')
+           $("#chat-room").removeClass('d-none')
+
+           //subscribe
+           stompClient.subscribe("/topic/return-to",function(response) {  //whatever response gone as response body parse it with json and pass it in function
+
+               showMessage(JSON.parse(response.body))
+           })
+
+       })
+}
+
+    //message will displaying in chatroom so design chatroom first
+function showMessage(message)
+{
+
+    $("#message-container-table").prepend(`<tr><td><b>${message.name} :</b> ${message.content}</td></tr>`)
+
+}
+
+
+
+$(document).ready((e)=>
+ {
+
+    $("#login").click(()=>{
+
+       let name=$("#name-value").val()
+         localStorage.setItem("name",name);
+
+       $("#name-title").html(`Welcome, <b>${name}</b>`)
+        connect();
+    })
+
+    $("#send-btn").click(()=>{
+        sendMessage()
+    })
+
+     $("#logout").click(()=>{
+
+         localStorage.removeItem("name")
+         if(stompClient!==null)
+         {
+             stompClient.disconnect()
+
+             $("#name-from").removeClass('d-none')
+             $("#chat-room").addClass('d-none')
+             console.log(stompClient)
+         }
+
+     })
+})
